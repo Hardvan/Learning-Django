f23ad66ba45141197a276023be20fc7d82aa981b
Hardik Pawar <hardikhpawar.cs21@rvce.edu.in>
2023-05-16 14:57:33 +0530

    Add __str__ method for Contact Model

--- home/models.py
+++ home/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 
 
+# * makemigrations - Create changes & Store in a file
+# * migrate - Apply the pending changes created by makemigrations
+
 # Create your models here.
 class Contact(models.Model):
 
@@ -9,3 +12,7 @@ class Contact(models.Model):
     phone = models.CharField(max_length=12)
     desc = models.TextField()
     date = models.DateField()
+
+    # Display the name of the object in the admin panel
+    def __str__(self):
+        return f"{self.name} - {self.email}"
