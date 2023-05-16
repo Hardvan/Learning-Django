8edb78b678f199b986b9690f35282dc2e3273886
Hardik Pawar <hardikhpawar.cs21@rvce.edu.in>
2023-05-16 14:49:55 +0530

    Create model for Contact

--- Hello/settings.py
+++ Hello/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'home.apps.HomeConfig',  # This is the app that we created
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
