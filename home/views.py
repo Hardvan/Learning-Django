014daacbedd9c0b0386c99be29399081f3ea3ee1
Hardik Pawar <hardikhpawar.cs21@rvce.edu.in>
2023-05-16 14:54:03 +0530

    Save the form data to database

--- home/views.py
+++ home/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, HttpResponse
+from datetime import datetime
+from home.models import Contact
 
 # render is used for static files
 
@@ -30,5 +32,19 @@ def services(request):
 
 
 def contact(request):
+
+    if request.method == "POST":
+
+        name = request.POST.get("name")
+        email = request.POST.get("email")
+        phone = request.POST.get("phone")
+        desc = request.POST.get("desc")
+        date = datetime.today()
+
+        contact = Contact(name=name, email=email,
+                          phone=phone, desc=desc, date=date)
+
+        # Save the contact object to the database
+        contact.save()
 
     return render(request, "contact.html")
