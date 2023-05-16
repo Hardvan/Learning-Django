84e22edab0803447e8f42907775de9e8fa3fb809
Hardik Pawar <hardikhpawar.cs21@rvce.edu.in>
2023-05-16 09:19:48 +0530

    Change admin text for header, title

--- Hello/urls.py
+++ Hello/urls.py
@@ -17,6 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+admin.site.site_header = "Hardvan IceCream Admin"
+admin.site.site_title = "Hardvan IceCream Admin Portal"
+admin.site.index_title = "Welcome to Hardvan IceCream"
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', include("home.urls"))  # Send all requests to home.urls
